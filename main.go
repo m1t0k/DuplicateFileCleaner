c721292411e48f4fad779c31d697422a7e2e457a
m1t0k <yuriy.matveychuk@gmail.com>
2017-02-03 17:45:30 +0300

    add support for multiple directories & patterns  + parallel directories scan

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"sync"
 )
 
@@ -17,11 +18,16 @@ type FileItems struct {
 	Files []string
 }
 
-// GlobalFileStorage holds all information about duplicate files
-var GlobalFileStorage = struct {
+// GlobalDuplicateFileList holds all information about duplicate files
+var GlobalDuplicateFileList = struct {
 	sync.RWMutex
 	FileStorage map[string]FileItems
 }{FileStorage: make(map[string]FileItems)}
+
+var GlobalFileList = struct {
+	sync.RWMutex
+	FileList []string
+}{}
 
 func md5sum(filePath string) (result string, err error) {
 	file, err := os.Open(filePath)
@@ -49,65 +55,97 @@ func processFile(fileName string, wg *sync.WaitGroup) {
 		//log.Printf("Error: can't calculate hash for file %s.\n Error:%v", fileName, errHash)
 		return
 	}
-	GlobalFileStorage.Lock()
-	item, ok := GlobalFileStorage.FileStorage[hash]
+	GlobalDuplicateFileList.Lock()
+	item, ok := GlobalDuplicateFileList.FileStorage[hash]
 	if ok == true {
 		item.Files = append(item.Files, fileName)
-		GlobalFileStorage.FileStorage[hash] = item
+		GlobalDuplicateFileList.FileStorage[hash] = item
 	} else {
 		var fileItems FileItems
-		fileItems.Files = append(GlobalFileStorage.FileStorage[hash].Files, fileName)
-		GlobalFileStorage.FileStorage[hash] = fileItems
+		fileItems.Files = append(GlobalDuplicateFileList.FileStorage[hash].Files, fileName)
+		GlobalDuplicateFileList.FileStorage[hash] = fileItems
 	}
-	GlobalFileStorage.Unlock()
+	GlobalDuplicateFileList.Unlock()
 }
 
-func findDuplicates(directory string, filePattern string) {
-	subDir := [2]string{"**", ""}
+func scanDirectory(dir string, pattern string, wg *sync.WaitGroup) {
+	defer (*wg).Done()
+
 	var files []string
+	subDir := [2]string{"**", ""}
+
 	for _, sub := range subDir {
-		dirFiles, err := filepath.Glob(directory + "\\" + sub + "\\" + filePattern)
+		fullDir := dir + "\\" + sub + "\\" + pattern
+		dirFiles, err := filepath.Glob(fullDir)
 		if err != nil {
-			log.Fatalf("Error: can't scan folder %s with pattern %s.\n Error:%v", directory, filePattern, err)
+			//log.Fatalf("Error: can't scan folder %s with pattern %s.\n Error:%v", directory, filePattern, err)
 			return
 		}
-		log.Printf("Found %d files in %s", len(dirFiles), directory)
+		log.Printf("Found %d files in %s", len(dirFiles), fullDir)
 		files = append(files, dirFiles...)
 	}
-	var wg sync.WaitGroup
-	wg.Add(len(files))
-	for index := 0; index < len(files); index++ {
-		go processFile(files[index], &wg)
-	}
-	wg.Wait()
+	GlobalFileList.Lock()
+	GlobalFileList.FileList = append(GlobalFileList.FileList, files...)
+	GlobalFileList.Unlock()
 
+}
+
+func findDuplicates(dirList []string, filePatterns []string) {
+	var dirWg sync.WaitGroup
+	dirWg.Add(len(dirList) * len(filePatterns))
+	for _, dir := range dirList {
+		for _, pattern := range filePatterns {
+			go scanDirectory(dir, pattern, &dirWg)
+		}
+	}
+	dirWg.Wait()
+	var fileWg sync.WaitGroup
+	fileWg.Add(len(GlobalFileList.FileList))
+	for index := 0; index < len(GlobalFileList.FileList); index++ {
+		go processFile(GlobalFileList.FileList[index], &fileWg)
+	}
+	fileWg.Wait()
 }
 
 func showResults() {
-	GlobalFileStorage.Lock()
-	fmt.Printf("%d\n", len(GlobalFileStorage.FileStorage))
-	for k, v := range GlobalFileStorage.FileStorage {
+	GlobalDuplicateFileList.Lock()
+	fmt.Printf("%d\n", len(GlobalDuplicateFileList.FileStorage))
+	for _, v := range GlobalDuplicateFileList.FileStorage {
 		if len(v.Files) > 1 {
-			fmt.Printf("key[%s] value[%s]\n", k, v)
+			fmt.Printf("value[%s]\n", v)
 		}
 	}
-	GlobalFileStorage.Unlock()
+	GlobalDuplicateFileList.Unlock()
 }
 
 type configurationSettings struct {
-	directory   string
-	filePattern string
+	dirList      []string
+	filePatterns []string
 }
 
 func getConfigSettings() configurationSettings {
 	var settings configurationSettings
-
-	flag.StringVar(&settings.directory, "dir", "", "-dir=C:\\downloads")
-	flag.StringVar(&settings.filePattern, "ext", "*.*", "-ext=*.pdf")
+	var dirs string
+	var files string
+	flag.StringVar(&dirs, "dir", "", "-dir=dir,dir2")
+	flag.StringVar(&files, "ext", "*.*", "-ext=*.ext1,*.ext2")
 	flag.Parse()
 
-	if len(settings.directory) <= 0 {
-		log.Fatal("Root directory is not set:-dir=C:\\downloads")
+	if len(dirs) <= 0 {
+		log.Fatal("Root directory is not set:-dir=C:\\dir1,dir2")
+	}
+
+	settings.dirList = strings.Split(dirs, ",")
+	settings.filePatterns = strings.Split(files, ",")
+
+	isSet := false
+	for _, dir := range settings.dirList {
+		if len(dir) > 0 && isSet == false {
+			isSet = true
+		}
+	}
+	if isSet == false {
+		log.Fatal("Root directory is not set:-dir=C:\\dir1,dir2")
 	}
 
 	return settings
@@ -115,6 +153,6 @@ func getConfigSettings() configurationSettings {
 
 func main() {
 	settings := getConfigSettings()
-	findDuplicates(settings.directory, settings.filePattern)
+	findDuplicates(settings.dirList, settings.filePatterns)
 	showResults()
 }
